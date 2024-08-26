2dd5260bd7cceadec7e316bdda9f080ddf054520
zain_moneer <zain_moneer@ZAIN-M0NEER>
2024-08-26 18:39:14 +0300

    Add project files.

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentCotnroller.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult getAll()
+        {
+            return View(getAll);
+        }
+        public IActionResult create()
+        {
+            return View(create);
+        }
+        public IActionResult update()
+        {
+            return View(update);
+        }
+        public IActionResult delet()
+        {
+            return View(delet);
+        }
+    }
+}
